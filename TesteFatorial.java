7d5154d1f0ec42e4b57572b6d8368583abdb3620
Fabio Santos Alves <fa3io.santos.alves@gmail.com>
2014-08-31 22:33:56 -0300

    Mudanca de variavel e mudaca de parametro

--- TesteFatorial.java
+++ TesteFatorial.java
@@ -2,7 +2,7 @@ class TesteFatorial{
 	//Comentario inserido em TesteFatorial.java
 	public static void main(String [] args){
 		Fatorial fatorial = new Fatorial();
-		int resultado = fatorial.calcularFatorial(0);
-		System.out.println(resultado);
+		int r = fatorial.calcularFatorial(10);
+		System.out.println(r);
 	}
 }
