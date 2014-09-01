0cf666d8316b994f430ef1abd63cee9095849ee4
Fabio Santos Alves <fa3io.santos.alves@gmail.com>
2014-08-31 22:32:16 -0300

    Mudanca de parametro para 4

--- TesteFatorial.java
+++ TesteFatorial.java
@@ -2,7 +2,7 @@ class TesteFatorial{
 	//Comentario inserido em TesteFatorial.java
 	public static void main(String [] args){
 		Fatorial fatorial = new Fatorial();
-		int resultado = fatorial.calcularFatorial(0);
+		int resultado = fatorial.calcularFatorial(4);
 		System.out.println(resultado);
 	}
 }
